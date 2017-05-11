cdb19b1cf4a34da61d1200b21d2e7b697b8ec4a0
prashantskit123 <prashantskit@gmail.com>
2017-05-11 07:38:27 +0000

    Code updated

--- server.js
+++ server.js
@@ -5,7 +5,34 @@ var app = express();
 app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/mittens');
 
+app.use(express.static('public')); 
+
+var Meow = mongoose.model('Meow', {
+ text: String
+  });
+
+app.get('/meows',function(req, res, next){
+  Meow.find({}, function(err, meows){
+      return res.json(meows);
+  });
+});
+
+app.post('/meows',function(req, res, next){
+  var newMeow=new Meow({
+    text:req.body.newMeow
+  });
+  newMeow.save(function(err){
+    return res.send("Add Successfully");
+  });
+});
+
+app.put('/meows/remove', function(req,res,next){
+var meowId=req.body.meow._id;
+    Meow.remove({_id:meowId},function(err){
+          return res.send("Deleted Successfully");
+    });
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
-
